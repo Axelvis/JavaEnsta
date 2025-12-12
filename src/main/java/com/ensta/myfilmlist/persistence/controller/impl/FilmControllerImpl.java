823e417fdd64fdd4e6ca6be011f4ef5d76cb2100
Bun <dusoleil01@hotmail.fr>
2025-12-12 10:52:01 +0100

    first update + adding cors policy

--- src/main/java/com/ensta/myfilmlist/persistence/controller/impl/FilmControllerImpl.java
+++ src/main/java/com/ensta/myfilmlist/persistence/controller/impl/FilmControllerImpl.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ import com.ensta.myfilmlist.persistence.controller.FilmController;
 import com.ensta.myfilmlist.service.MyFilmsService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/film")
 public class FilmControllerImpl implements FilmController {
 
--- src/main/java/com/ensta/myfilmlist/persistence/controller/impl/RealisateurControllerImpl.java
+++ src/main/java/com/ensta/myfilmlist/persistence/controller/impl/RealisateurControllerImpl.java
@@ -9,6 +9,7 @@ import com.ensta.myfilmlist.service.RealisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/realisateur")
 public class RealisateurControllerImpl implements RealisateurController {
 
