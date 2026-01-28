192237e6f7051d6fa9f4d27baa8c0c6c322dc354
GuillaumeR1 <robin.guillaume@gmx.com>
2026-01-28 09:51:56 +0100

    Clean RealisateurMapper
    Removed comments for the RealisateurMapper class and its method.

--- src/main/java/com/ensta/myfilmlist/mapper/RealisateurMapper.java
+++ src/main/java/com/ensta/myfilmlist/mapper/RealisateurMapper.java
@@ -3,18 +3,10 @@ package com.ensta.myfilmlist.mapper;
 import com.ensta.myfilmlist.form.RealisateurForm;
 import com.ensta.myfilmlist.model.Realisateur;
 
-/**
- * Mapper statique pour la conversion entre RealisateurForm et Realisateur (Model).
- */
 public final class RealisateurMapper {
 
     private RealisateurMapper() { }
 
-    /**
-     * Convertit un RealisateurForm en entité Realisateur.
-     * * @param form Le formulaire contenant les données.
-     * @return L'entité Realisateur créée.
-     */
     public static Realisateur convertRealisateurFormToRealisateur(RealisateurForm form) {
         Realisateur realisateur = new Realisateur();
         realisateur.setNom(form.getNom());
